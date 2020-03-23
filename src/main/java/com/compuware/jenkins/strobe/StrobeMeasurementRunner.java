501df49787bfc155d66bb7add7f2839e15b4aa18
VinceCapodanno <vince.capodanno@compuware.com>
2020-03-23 13:41:04 -0400

    Fixed bug and updated pom

--- src/main/java/com/compuware/jenkins/strobe/StrobeMeasurementRunner.java
+++ src/main/java/com/compuware/jenkins/strobe/StrobeMeasurementRunner.java
@@ -1,14 +1,14 @@
 /**
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 - 2019 Compuware Corporation
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
  * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice
  * shall be included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
@@ -34,23 +34,23 @@ import hudson.model.TaskListener;
 import hudson.util.Secret;
 
 public class StrobeMeasurementRunner
-{	
+{
 	private final StrobeMeasurementBuilder smBuilder;
-	
+
 	/**
 	 * Constructor
 	 * 
 	 * @param smBuilder
 	 * 			  An instance of <code>StrobeMeasurementBuilder</code> containing the arguments.
 	 */
-	public StrobeMeasurementRunner(StrobeMeasurementBuilder smBuilder)
+	public StrobeMeasurementRunner(final StrobeMeasurementBuilder smBuilder)
 	{
 		this.smBuilder = smBuilder;
 	}
-	
+
 	/**
 	 * Submits the Strobe measurement
-	 * 
+	 *
 	 * @param build
 	 *			  The current running Jenkins build
 	 * @param launcher
@@ -59,9 +59,9 @@ public class StrobeMeasurementRunner
 	 *            a directory to check out the source code.
 	 * @param listener
 	 *            Build listener
-	 *            
+	 *
 	 * @return <code>boolean</code> if the build was successful
-	 * 
+	 *
 	 * @throws IOException
 	 * 			If an error occurred during the Strobe measurement submit
 	 */
@@ -72,9 +72,9 @@ public class StrobeMeasurementRunner
 		HttpPost post = new HttpPost(url);
 
 		// do this so that we will try and send an email since the notification code requires us to attempt to send an email
-		String DEFAULT_EMAIL = "dummy@compuware.com"; 
+		String DEFAULT_EMAIL = "dummy@compuware.com";
 		String emailToUse = smBuilder.getEmailto().isEmpty() ? DEFAULT_EMAIL : StringUtils.trimToEmpty(smBuilder.getEmailto());
-				
+
         StringBuilder json = new StringBuilder();
         json.append("{");
         json.append("\"reqType\":\"" + StringUtils.trimToEmpty(smBuilder.getRequestType()) + "\",");
@@ -110,44 +110,47 @@ public class StrobeMeasurementRunner
 			listener.getLogger().println("No callback configured");
         }
         json.append("}");
-        
+
         post.setEntity(new StringEntity(json.toString()));
         post.addHeader("Content-Type", "application/json");
-        post.addHeader("Authorization", token.getPlainText());
-        
+
+        if (token != null) {
+            post.addHeader("Authorization", token.getPlainText());
+        }
+
         int returnCode = 99999;
         try (CloseableHttpClient httpClient = HttpClients.createDefault();
                 CloseableHttpResponse response = httpClient.execute(post))
         {
-        	String results = EntityUtils.toString(response.getEntity());        	
+        	String results = EntityUtils.toString(response.getEntity());
 			returnCode = getReturnCode(results);
 			listener.getLogger().println("Return Code=" + returnCode);
 			listener.getLogger().println("Session Request Number=" + getSessionRequestNumber(results));
         }
-        
+
 		return returnCode < 5; // got the <5 criteria from the processResponse function in StrobeService.java
 	}
 
 	// instead of importing dependencies and other files to parse this json result into an object, we just process the string
 	private int getReturnCode(String results) {
 		String returnCodeString = "returnCode\":";
-		int returnCodeIndex = results.indexOf(returnCodeString);	
-		int commaIndex = results.indexOf(",", returnCodeIndex);		
+		int returnCodeIndex = results.indexOf(returnCodeString);
+		int commaIndex = results.indexOf(",", returnCodeIndex);
 		String returnCode = results.substring(returnCodeIndex + returnCodeString.length(), commaIndex);
 		return Integer.parseInt(returnCode);
 	}
-	
+
 	private int getSessionRequestNumber(String results) {
 		String sessionRequestNumberString = "@number\":";
 		int sessionRequestNumberIndex = results.indexOf(sessionRequestNumberString);
 		int commaIndex = results.indexOf(",", sessionRequestNumberIndex);
-		String sessionRequestNumber = results.substring(sessionRequestNumberIndex + sessionRequestNumberString.length(), commaIndex);    
-		
+		String sessionRequestNumber = results.substring(sessionRequestNumberIndex + sessionRequestNumberString.length(), commaIndex);
+
 		// sometimes theres quotes around the number and sometimes not
 		if (sessionRequestNumber.length() == 6) {
 			sessionRequestNumber = sessionRequestNumber.substring(1, sessionRequestNumber.length() - 1);
 		}
-		
+
 		return Integer.parseInt(sessionRequestNumber);
 	}
 
@@ -162,7 +165,8 @@ public class StrobeMeasurementRunner
 		        
 		        if (!headerJson.toString().equals("")) {
 		            headerJson.append(",");
-		        }	        
+		        }
+		        
 		        headerJson.append("{");
 		        headerJson.append("\"name\":\"" + keyValuePair[0] + "\"");
 		        headerJson.append(",");
@@ -170,7 +174,7 @@ public class StrobeMeasurementRunner
 		        headerJson.append("}");
 		    }
 	    }
-    
+
 	    return headerJson.toString();
 	}
 }
